da55bb6cffab5bfdabb2592bfb1ed016f27452f7
Hopecream.ZHANG <Hopecream.ZHANG@177043a6-089c-11de-b5bf-434079a3519f>
2009-12-29 13:08:49 +0000

    Modify the timer0.c

--- timer/timer0.c
+++ timer/timer0.c
@@ -1,36 +1,51 @@
 #include <stdio.h>
+#include <math.h>
 
 unsigned long long get_rdtsc()
 {
-    asm("rdtsc");
+    unsigned long long ret;
+
+    __asm__ __volatile__ ("rdtsc":"=A"(ret));
+
+    return ret;
 }
 
 int main()
 {
-    unsigned long long i, t1, t2;
-    long double freq;
+    int i;
+    unsigned long long t1, t2;
+    double freq;
     FILE *fin;
-    char *line;
-    int length;
-
+    char line[255];
     fin = fopen("/proc/cpuinfo", "r");
-    while(getline(&line, &length, fin) != EOF)
+    if(!fin)
     {
-        if(sscanf(line, "cpu MHz\t\t: %llf", &freq) == 1)
+        printf("\n");
+        return 1;
+    }
+    while(fgets(line, 255, fin) != NULL)
+    {
+        if(sscanf(line, "cpu MHz\t\t: %lf", &freq) == 1)
         {
             freq *= 1000000ULL;
             break;
         }
     }
-    close(fin);
-    printf("cpu frequency = %lf\n", freq);
+    if(fabs(freq - 0) < 1e-9)
+    {
+        printf("Error\n");
+        return 1;
+    }
+
+    fclose(fin);
+    printf("cpu frequency = %lfHz\n", freq);
 
     for(i = 0; i < 10; i ++)
     {
         t1 = get_rdtsc();
         sleep(1);
         t2 = get_rdtsc();
-        printf("t2 - t1 = %lld (%lldMHZ), time = %llf\n ", t2 - t1, (t2-t1)/1000000, ((double)(t2 - t1) / freq) * 1000);
+        printf("t2 - t1 = %llu, time = %lfus\n ", t2 - t1, ((double)(t2 - t1) / freq) * 1000);
     }
     return 0;
 }
